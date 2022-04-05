11013c368fc95be2b8f8a2ab4ae617e2693a4cea
nursebil canbolat <nursebiltw@gmail.com>
2022-04-06 01:15:18 +0300

    Stack completed

---
+++ Stack/LinkedListStack.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using SinglyLinkedList;
+
+namespace Stack
+{
+    public class LinkedListStack<T> : IStack<T>
+    {
+        private readonly SinglyLinkedList<T> list
+             = new SinglyLinkedList<T>();
+        public int Count { get; private set; }
+
+        public T Peek()
+        {
+            if (Count == 0)
+                throw new Exception("Empty stack");
+            return list.Head.Value;
+        }
+
+        public T Pop()
+        {
+            if (Count == 0)
+                throw new Exception("Empty stack");
+            var temp = list.RemoveFirst();
+            Count--;
+            return temp;
+        }
+
+        public void Push(T value)
+        {
+            list.AddFirst(value);
+            Count++;
+        }
+    }
+}
---
+++ StackConsoleApp/Program.cs
@@ -0,0 +1,34 @@
+﻿using Stack;
+using System;
+
+namespace StackConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var charset = new char[] { 'a', 'b', 'c', 'd', 'e' };
+            var stack1 = new Stack<char>();
+            var stack2 = new Stack<char>(StackType.LinkedList);
+
+            foreach (var item in charset)
+            {
+                Console.WriteLine(item);
+                stack1.Push(item);
+                stack2.Push(item);
+            }
+            Console.WriteLine($"Stack1 Peek: {stack1.Peek()}");
+            Console.WriteLine($"Stack2 Peek: {stack2.Peek()}");
+            Console.WriteLine();
+          
+            Console.WriteLine($"Stack1 Count: {stack1.Count}");
+            Console.WriteLine($"Stack2 Count: {stack2.Count}");
+            Console.WriteLine();
+            
+            Console.WriteLine($"Stack1 Pop: {stack1.Pop()}");
+            Console.WriteLine($"Stack2 Pop: {stack2.Pop()}");
+
+            Console.ReadKey();
+        }
+    }
+}
